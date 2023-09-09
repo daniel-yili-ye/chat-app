80411af203cef295f05c694d0703094b7d0f2430
Daniel Ye <danielye@Daniels-Laptop.local>
2023-09-09 14:01:08 -0400

    added button

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button>hello world</Button>
+}
+
+export default page
